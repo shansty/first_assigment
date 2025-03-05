7bb7ba7aa5ffe3d0e2eabd5ada93da511081c615
Anastasiya <shirochina16@gmail.com>
2025-03-05 19:07:36 +0100

    Add new useState for handling different css

--- front/src/pages/mainPage/components/NavigationMenu.tsx
+++ front/src/pages/mainPage/components/NavigationMenu.tsx
@@ -3,31 +3,23 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getCategoriesNames } from '../../../axios';
 
-const NavigationMenu = ({ onToggle }: { onToggle: (isOpen: boolean) => void }) => {
+const NavigationMenu = () => {
     const [categories, setCategories] = useState<String[]>([]);
     const [isOpen, setIsOpen] = useState(true)
 
     useEffect(() => {
-        // getCategoriesNames(setCategories);
+        getCategoriesNames(setCategories);
     }, [])
 
-    const handleToggle = () => {
-        setIsOpen(prev => {
-            const newState = !prev;
-            onToggle(newState);  
-            return newState;
-        });
-    };
-
     return  (
-        <div className={isOpen ? "nav_menu open" : "nav_menu closed"}>
-            <div className={isOpen ? "menu_button_open" : "menu_button_close"} onClick={handleToggle}>
+        <div className={isOpen ? "menu_open" : ""}>
+            <div className={isOpen ? "menu_button_open" : "menu_button_close"} onClick={() => setIsOpen(!isOpen)}>
                 {isOpen ? '✖' : '▶'}
             </div>
             {isOpen && <h2 className='menu_title'>Categories: </h2>}
             {isOpen && categories.map(category => (
                 <div className="category">
-                    <Link to={`/${category}`}><h3>{category}</h3></Link>
+                    <Link to={`/?category=${category}`}><h3>{category}</h3></Link>
                 </div>
             ))}
         </div>
@@ -35,4 +27,3 @@ const NavigationMenu = ({ onToggle }: { onToggle: (isOpen: boolean) => void }) =
 }
 
 export default NavigationMenu;
-
