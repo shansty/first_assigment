0f68d6893b6ac9ec66bc1bcd1d024d96b2646a4c
Anastasiya <shirochina16@gmail.com>
2025-03-05 09:26:55 +0100

    Add Dropdown and Logo components in utils_component folder

--- front/src/utils_components/Header.tsx
+++ front/src/utils_components/Header.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import logo from '../assets/logo.png';
-import profile from '../assets/user_p.png';
+import Logo from './Logo';
+import Dropdown from './Dropdown';
 import './utils.css'
 
 
 const Header: React.FC = () => {
     return (
         <div className='header_container'>
-            <div className='logo'>
-                <a href='#' >
-                    <img src={logo} alt="Logo" className="logo_img"></img>
-                </a>
-            </div>
-            <div className="dropdown">
-                <button className="dropbtn">
-                    <img src={profile} alt="profile" className="profile_img"></img>
-                    <p className='profile_name'>Name LastName</p>
-                </button>
-                <div className="dropdown-content">
-                    <a href="#">Profile</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
-                </div>
-            </div>
+            <Logo />
+            <Dropdown />
         </div>
     );
 }
