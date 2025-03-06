72956c88d27a3b797771e80ffe5112193d546bd5
Anastasiya <shirochina16@gmail.com>
2025-03-06 11:43:41 +0100

    Refactor Css for vertical nav menu

--- front/src/pages/mainPage/components/NavigationMenu.tsx
+++ front/src/pages/mainPage/components/NavigationMenu.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getCategoriesNames } from '../../../axios';
 
-const NavigationMenu = () => {
+const NavigationMenu = ({ onToggle }: { onToggle: (isOpen: boolean) => void }) => {
     const [categories, setCategories] = useState<String[]>([]);
     const [isOpen, setIsOpen] = useState(true)
 
@@ -11,9 +11,17 @@ const NavigationMenu = () => {
         getCategoriesNames(setCategories);
     }, [])
 
+    const handleToggle = () => {
+        setIsOpen(prev => {
+            const newState = !prev;
+            onToggle(newState);  
+            return newState;
+        });
+    };
+
     return  (
-        <div className={isOpen ? "menu_open" : ""}>
-            <div className={isOpen ? "menu_button_open" : "menu_button_close"} onClick={() => setIsOpen(!isOpen)}>
+        <div className={isOpen ? "nav_menu open" : "nav_menu closed"}>
+            <div className={isOpen ? "menu_button_open" : "menu_button_close"} onClick={handleToggle}>
                 {isOpen ? '✖' : '▶'}
             </div>
             {isOpen && <h2 className='menu_title'>Categories: </h2>}
@@ -27,3 +35,4 @@ const NavigationMenu = () => {
 }
 
 export default NavigationMenu;
+
