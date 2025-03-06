f910aed5df69796ba12b005202e0bd4a6d65748b
Anastasiya <shirochina16@gmail.com>
2025-03-06 11:43:41 +0100

    Refactor Css for vertical nav menu

--- front/src/pages/mainPage/MainPage.tsx
+++ front/src/pages/mainPage/MainPage.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavigationMenu from './components/NavigationMenu';
 import MainContent from './components/MainContent';
-import './mainPage.css'
+import './mainPage.css';
 
 const MainPage = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
+
     return (
         <div className='main_page_container'>
-            <div className='nav_menu'>
-                <NavigationMenu />
-            </div>
-            <div className='main_content'>
+            <NavigationMenu onToggle={setIsMenuOpen} />
+            <div className={isMenuOpen ? "main_content expanded" : "main_content collapsed"}>
                 <MainContent />
             </div>
         </div>
     );
-}
+};
 
 export default MainPage;
