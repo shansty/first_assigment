3124aaf5fb61d47ea304af9ccd1051695654ddd6
Anastasiya <shirochina16@gmail.com>
2025-03-06 09:09:45 +0100

    Fixed backend getProductsByCategory and refactor NavigationMenu component by comments

--- front/src/pages/mainPage/components/NavigationMenu.tsx
+++ front/src/pages/mainPage/components/NavigationMenu.tsx
@@ -3,29 +3,23 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getCategoriesNames } from '../../../axios';
 
-const NavigationMenu = ({ onToggle }: { onToggle: (isOpen: boolean) => void }) => {
-    const [categories, setCategories] = useState<String[]>([]);
-    const [isOpen, setIsOpen] = useState(true)
-
+const NavigationMenu = () => {
+    const default_category = "all categories"
+    const [categories, setCategories] = useState<String[]>([default_category]);
+    
     useEffect(() => {
-        getCategoriesNames(setCategories);
+        setAllCategories();
     }, [])
 
-    const handleToggle = () => {
-        setIsOpen(prev => {
-            const newState = !prev;
-            onToggle(newState);  
-            return newState;
-        });
-    };
+    const setAllCategories = async () => {
+        const db_catedories = await getCategoriesNames();
+        setCategories([default_category, ...db_catedories])
+    }
 
-    return  (
-        <div className={isOpen ? "nav_menu open" : "nav_menu closed"}>
-            <div className={isOpen ? "menu_button_open" : "menu_button_close"} onClick={handleToggle}>
-                {isOpen ? '✖' : '▶'}
-            </div>
-            {isOpen && <h2 className='menu_title'>Categories: </h2>}
-            {isOpen && categories.map(category => (
+    return (
+        <div className='nav_menu_container'>
+            <h2>Categories: </h2>
+            {categories.map(category => (
                 <div className="category">
                     <Link to={`/?category=${category}`}><h3>{category}</h3></Link>
                 </div>
