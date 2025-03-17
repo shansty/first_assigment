9c4711ca1c5ac601149058fc2a21e9dd49ee9d65
Anastasiya <shirochina16@gmail.com>
2025-03-17 09:51:10 +0100

    Add product data and create a ProductPage component + css

--- back/src/controllers/product.ts
+++ back/src/controllers/product.ts
@@ -26,8 +26,8 @@ export const getProductData = async (req: Request, res: Response) => {
         let product_id = +req.params.id;
         console.log(product_id);
         const product = await getProductsByProductId(product_id);
-        console.log(product)
-        res.status(200).json({product: product})
+        console.log(product[0])
+        res.status(200).json({product: product[0]})
     } catch (error) {
         console.log(`Error ${error}`)
         res.status(500).json({ message: "Error getting list of products by category" })
