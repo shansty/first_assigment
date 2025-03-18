c44fc8e9df17c4492f2a2e57ad0f83324d956297
Anastasiya <shirochina16@gmail.com>
2025-03-18 11:39:25 +0100

    Delete search router and controller

--- front/src/configs/axios_urls.ts
+++ front/src/configs/axios_urls.ts
@@ -3,3 +3,4 @@ export const CATEGORY_URL = `${process.env.REACT_APP_AXIOS_HOST}/category`
 export const PRODUCT_URL = `${process.env.REACT_APP_AXIOS_HOST}/product`
 
 export const PRODUCT_SEARCH_URL = `${process.env.REACT_APP_AXIOS_HOST}/product?search=`
+
