1c12f2ffc3bd7fb6823a4d24591e6adf5ac0e7d5
Anastasiya <shirochina16@gmail.com>
2025-03-18 16:37:58 +0100

    Change code by wrong commits from Commit 9fd4d57 till Commit 1cbdabf

--- front/src/pages/mainPage/components/NavigationMenu.tsx
+++ front/src/pages/mainPage/components/NavigationMenu.tsx
@@ -3,23 +3,29 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getCategoriesNames } from '../../../axios';
 
-const NavigationMenu = () => {
-    const default_category = "all categories"
-    const [categories, setCategories] = useState<String[]>([default_category]);
-    
+const NavigationMenu = ({ onToggle }: { onToggle: (isOpen: boolean) => void }) => {
+    const [categories, setCategories] = useState<String[]>([]);
+    const [isOpen, setIsOpen] = useState(true)
+
     useEffect(() => {
-        setAllCategories();
+         // getCategoriesNames(setCategories);
     }, [])
 
-    const setAllCategories = async () => {
-        const db_catedories = await getCategoriesNames();
-        setCategories([default_category, ...db_catedories])
-    }
+    const handleToggle = () => {
+        setIsOpen(prev => {
+            const newState = !prev;
+            onToggle(newState);  
+            return newState;
+        });
+    };
 
-    return (
-        <div className='nav_menu_container'>
-            <h2>Categories: </h2>
-            {categories.map(category => (
+    return  (
+        <div className={isOpen ? "nav_menu open" : "nav_menu closed"}>
+            <div className={isOpen ? "menu_button_open" : "menu_button_close"} onClick={handleToggle}>
+                {isOpen ? '✖' : '▶'}
+            </div>
+            {isOpen && <h2 className='menu_title'>Categories: </h2>}
+            {isOpen && categories.map(category => (
                 <div className="category">
                    <Link to={`/${category}`}><h3>{category}</h3></Link>
                 </div>
