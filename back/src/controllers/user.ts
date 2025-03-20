c30c495ea157781987d455cf6bfedca184d5f6f6
Anastasiya <shirochina16@gmail.com>
2025-03-20 15:09:38 +0100

    Correct messages in user controller

--- back/src/controllers/user.ts
+++ back/src/controllers/user.ts
@@ -4,13 +4,21 @@ import { generateToken, getUserByQuery, updateUser, createUser } from './utils';
 
 export const register = async (req: Request, res: Response) => {
     const user = req.body.user;
-    if (user.password == "" || user.email == "" || user.first_name == "") {
-        res.status(404).json({ message: 'Requred data is empty' })
+    if (user.password == "") {
+        res.status(400).json({ message: 'Password field is empty' })
+        return;
+    }
+    if (user.email == "") {
+        res.status(400).json({ message: 'Email field is empty' })
+        return;
+    }
+    if (user.first_name == "") {
+        res.status(400).json({ message: 'First name field is empty' })
         return;
     }
     const already_registered_user = await getUserByQuery({ email: user.email });
     if (already_registered_user) {
-        res.status(404).json({ message: "User already registered" })
+        res.status(400).json({ message: "This email has already taken" })
     }
     const createdUser = await createUser(user);
     const token = generateToken(createdUser._id)
@@ -20,13 +28,17 @@ export const register = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
     const user = req.body.user;
-    if (user.password === "" || user.email === "") {
-        res.json({ message: 'Requred data is empty' })
+    if (user.password == "") {
+        res.status(400).json({ message: 'Password field is empty' })
+        return;
+    }
+    if (user.email == "") {
+        res.status(400).json({ message: 'Email field is empty' })
         return;
     }
     const loginUser = await getUserByQuery({ email: user.email, password: user.password });
     if (!loginUser) {
-        res.status(404).json({ message: "User not registered" });
+        res.status(400).json({ message: "Unable to find user with this credentials" });
         return;
     }
     const token = generateToken(loginUser._id)
@@ -38,7 +50,7 @@ export const getUserData = async (req: Request, res: Response) => {
     const user_id = req.params.user_id;
     const user = await getUserByQuery({ _id: user_id });
     if (!user) {
-        res.status(404).json({ message: "User data not found" });
+        res.status(400).json({ message: "Unable to find user with this credentials" });
         return;
     }
     res.status(200).json({ user: user })
@@ -50,7 +62,7 @@ export const editUserData = async (req: Request, res: Response) => {
     const user_data = req.body.user;
     const edited_user = await updateUser(user_id, user_data);
     if (!edited_user) {
-        return res.status(404).json({ message: "User not found" });
+        return res.status(400).json({ message: "Unable to find user with this credentials" });
     }
     res.status(200).json({ edited_user: edited_user })
 }
